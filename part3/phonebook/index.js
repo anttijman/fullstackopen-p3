c651e56b6f2d498e903f49ff0ebc77ac6645af76
anttijman <63282763+anttijman@users.noreply.github.com>
2025-04-17 10:13:45 +0300

    Remove cors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const cors = require('cors')
-
 var morgan = require('morgan')
 const app = express()
-app.use(cors())
 app.use(express.static('dist'))
 
 let persons = [
